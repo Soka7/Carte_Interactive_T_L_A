117d23ed16a7fd3449ab54384955253b7d0da8bf
Yolked64 <leobodinleo@gmail.com>
2026-01-07 16:57:26 +0100

    Prepared merge
    Update the branch to be as updated as main, everything is ready in it, ready to merge

---
+++ Carte_interactive.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2deb2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : mer. 07 jan. 2026 à 11:10
+-- Version du serveur : 8.4.7-0ubuntu0.25.10.3
+-- Version de PHP : 8.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `Carte_interactive`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Cameras`
+--
+
+CREATE TABLE `Cameras` (
+  `id_camera` int NOT NULL,
+  `coordonnees` text,
+  `lien_photo` text,
+  `origin_user` int DEFAULT NULL,
+  `verifie` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Login`
+--
+
+CREATE TABLE `Login` (
+  `id` int NOT NULL,
+  `pseudo` text,
+  `mdp` text,
+  `email` text,
+  `admin` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Cameras`
+--
+ALTER TABLE `Cameras`
+  ADD PRIMARY KEY (`id_camera`);
+
+--
+-- Index pour la table `Login`
+--
+ALTER TABLE `Login`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- js/app1.js
+++ js/app1.js
@@ -11,9 +11,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // insertion du marqueur pour le Miroir d'eau
-let marker = L.marker([47.2150, -1.5491]);
+let marker = L.marker([47.2150, -1.5491], {title: "More info",});
     // ajout à la carte
-marker.addTo(map);
+marker.addTo(map).bindPopup("<div class = pop><div class = 'pop_text'><h1>Miroir d'eau</h1><p>Le miroir d'eau de Nantes est une pièce d'eau peu profonde située dans le centre-ville de Nantes.</p><br><video controls autoplay><source src='Nantes.mp4'></source></video><br><p>The video may occur to be unavailable. it is NOT a code error and may be occured by bad video quality or bad engine. We take NO reponsability about that.</p></div>");
     // bulle avec texte
 marker.bindTooltip("Miroir d'eau", {
    direction: "top",
@@ -21,3 +21,38 @@ marker.bindTooltip("Miroir d'eau", {
    offset: [-15,-15], // on décale un peu la bulle vers le haut et à gauche,
    opacity: 0.6 // semi transparente
 }).openTooltip();
+
+function onMapClick(e) {
+    let marker = L.marker(e.latlng, {title: "More info",});
+        // ajout à la carte
+    marker.addTo(map);
+        // bulle avec texte
+    marker.bindTooltip(e.latlng.toString(), {
+    direction: "top",
+    permanent: true,
+    offset: [-15,-15], // on décale un peu la bulle vers le haut et à gauche,
+    opacity: 0.6 // semi transparente
+    }).openTooltip();
+
+    var circle = L.circle(e.latlng, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 10
+    }).addTo(map);
+}
+
+//...
+
+map.on('click', onMapClick);
+
+const map_ = document.getElementById("map")
+const btn = document.querySelector(".Expand")
+
+btn.addEventListener("click", () => {
+    map_.classList.toggle("expand")
+    map.invalidateSize()
+    btn.classList.toggle("ex")
+})
+
+//ChatGPT utilise pour mieux comprendre la source des erreurs.
