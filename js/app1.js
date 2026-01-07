d7f98abaa8c3e67ee0bcc55bc4ca92e9341c4926
jesus-dev7 <kids.anguita@gmail.com>
2026-01-07 14:29:12 +0100

    Hover effect.

--- js/app1.js
+++ js/app1.js
@@ -11,7 +11,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // insertion du marqueur pour le Miroir d'eau
-let marker = L.marker([47.2150, -1.5491]);
+let marker = L.marker([47.2150, -1.5491], {title: "More info",});
     // ajout à la carte
 marker.addTo(map);
     // bulle avec texte
@@ -23,7 +23,7 @@ marker.bindTooltip("Miroir d'eau", {
 }).openTooltip();
 
 function onMapClick(e) {
-    let marker = L.marker(e.latlng);
+    let marker = L.marker(e.latlng, {title: "More info",});
         // ajout à la carte
     marker.addTo(map);
         // bulle avec texte
