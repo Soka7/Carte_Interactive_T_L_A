858ef8c7a3191e5fa1e9c674e6d023dd0415ac51
Yolked64 <leobodinleo@gmail.com>
2026-01-08 17:34:33 +0100

    Account
    Added Account creation
    Changed login menu
    Fixed some log in bug

--- TraitementLogin.php
+++ TraitementLogin.php
@@ -1,19 +1,18 @@
 <?php
-$login = $_GET["login"];
-$mail = $_GET["mail"];
-$mdp = $_GET["mdp"];
+$login = $_POST["login"];
+$mdp = $_POST["mdp"];
 
 $DataBase = new PDO('mysql:host=localhost;port=3306;dbname=carte_interactive;charset=utf8','root','ChuckNorris44');
 
 $RequestValidity = $DataBase->prepare("SELECT COUNT(*) AS counter FROM login WHERE pseudo=? AND mdp=?");
 $RequestValidity->execute([$login, $mdp]);
-$Validity = $RequestValidity->fetchAll(PDO::FETCH_ASSOC);
+$Validity = $RequestValidity->fetch();
 
 $RequestAdmin = $DataBase->prepare("SELECT admin AS adm FROM login WHERE pseudo=? AND mdp=?");
 $RequestAdmin->execute([$login, $mdp]);
 $AdminUser = $RequestAdmin->fetch();
 
-if($Validity["counter"] == 1 || $AdminUser["adm"] == 1)
+if($Validity["counter"] == 1 && $AdminUser["adm"] == 1)
 {
     header('Location: AdminView.html');
 }
